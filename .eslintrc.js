3f84eb9339c2840f9b97b6e2319b037bd21c9344
Mathis Lefranc <lefrancmathis@gmail.com>
2024-02-05 18:06:14 -0400

    Updated linting 3

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,8 +16,8 @@ module.exports = {
   rules: {
     indent: ["error", 2],
     "linebreak-style": ["error", "unix"],
-    quotes: ["error", "double", "single"],
-    semi: ["error", "never"],
+    // quotes: ["error", "double", "single"],
+    // semi: ["error", "never"],
     eqeqeq: "error",
     "no-trailing-spaces": "error",
     "object-curly-spacing": ["error", "always"],

adf1b845d3d83cf35568f2e01e06a97fae03c503
Mathis Lefranc <lefrancmathis@gmail.com>
2024-02-05 18:11:04 -0400

    Updated linting 4

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
+// eslint-disable-next-line no-undef
 const express = require("express");
 const app = express();
 
 // get the port from env variable
+// eslint-disable-next-line no-undef
 const PORT = process.env.PORT || 3001;
 
 app.use(express.static("dist"));

d9171b3c4286e3eb41b3d94651ecf71e5700883b
Mathis Lefranc <lefrancmathis@gmail.com>
2024-03-19 15:24:28 -0400

    fixed no undef linting error

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter as Router } from 'react-router-dom'
-import App from './App'
-import './styles.css'
-
-ReactDOM.createRoot(document.getElementById('app')).render(
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+import App from "./App";
+import "./styles.css";
+// eslint-disable-next-line no-undef
+ReactDOM.createRoot(document.getElementById("app")).render(
   <Router>
     <App />
   </Router>
-)
+);
